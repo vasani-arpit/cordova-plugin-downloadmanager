b9486ce911202fec0e61e327ac982bce04608853
slidenergy <slidenergy@gmail.com>
2017-10-19 18:09:56 +1000

    Added addCompletedDownload method call for the case when the file has already been downloaded

--- www/DownloadManager.js
+++ www/DownloadManager.js
@@ -3,3 +3,7 @@ var exec = require('cordova/exec');
 exports.download = function(arg0, success, error) {
     exec(success, error, "DownloadManager", "download", [arg0]);
 };
+
+exports.addCompletedDownload = function(title, description, mimeType, path, length, success, error) {
+    exec(success, error, "DownloadManager", "addCompletedDownload", [title, description, mimeType, path, length]);
+};
