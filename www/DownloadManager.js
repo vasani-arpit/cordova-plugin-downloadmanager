22de7de5e532c03481b2126d1217a2379de17679
Diogo Correia <diogo.correia@inm.pt>
2019-12-13 08:06:24 +0000

    Adding filename parameter

--- www/DownloadManager.js
+++ www/DownloadManager.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.download = function(arg0, success, error) {
-    exec(success, error, "DownloadManager", "download", [arg0]);
+exports.download = function(arg0, arg1, success, error) {
+    exec(success, error, "DownloadManager", "download", [arg0, arg1]);
 };
